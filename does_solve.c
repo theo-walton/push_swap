2873e1932757835a39a0d0cc5c8e5b5e025c5c15
Theo Walton <twalton@e1z1r6p24.42.us.org>
2017-07-04 23:07:11 -0700

    almost done part 1

--- does_solve.c
+++ does_solve.c
@@ -10,12 +10,67 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
+static void	perform_action2(char *action, int *brr, int *arr)
+{
+	if (!ft_strcmp(action, "rb"))
+		rot_elem(brr, '+');
+	else if (!ft_strcmp(action, "rr"))
+	{
+		rot_elem(arr, '+');
+		rot_elem(brr, '+');
+	}
+	else if (!ft_strcmp(action, "rra"))
+		rot_elem(arr, '-');
+	else if (!ft_strcmp(action, "rrb"))
+		rot_elem(brr, '-');
+	else if (!ft_strcmp(action, "rrr"))
+	{
+		rot_elem(arr, '-');
+		rot_elem(brr, '-');
+	}
+}
 
 static void	perform_action(char *action, int *brr, int *arr)
 {
 	if (!ft_strcmp(action, "sa"))
 		swap_elem(arr);
+	else if (!ft_strcmp( action, "sb"))
+		swap_elem(brr);
+	else if (!ft_strcmp(action, "ss"))
+	{
+		swap_elem(arr);
+		swap_elem(brr);
+	}
+	else if (!ft_strcmp(action, "pa"))
+		push_elem(arr, brr);
+	else if (!ft_strcmp(action, "pb"))
+		push_elem(brr, arr);
+	else if (!ft_strcmp(action, "ra"))
+		rot_elem(arr, '+');
+	else
+		perform_action2(action, brr, arr);
+}
+
+static void	check_if_solved(int *brr, int *arr)
+{
+	int i;
+
+	i = 1;
+	if (brr[0])
+	{
+		write(1, "KO\n", 3);
+		return ;
+	}
+	while (i < arr[0])
+	{
+		if (arr[i] >= arr[i + 1])
+		{
+			write(1, "KO\n", 3);
+			return ;
+		}
+		i++;
+	}
+	write(1, "OK\n", 3);
 }
 
 void    does_solve(char **strarr, int *arr)
@@ -26,9 +81,10 @@ void    does_solve(char **strarr, int *arr)
 	i = 0;
 	if (str == NULL)
 		return ;
-	brr = ft_memalloc(sizeof(int) * arr[0]);
+	brr = ft_memalloc(sizeof(int) * (arr[0] + 1));
+	brr[0] = 0;
 	while (strarr[i])
 		perform_action(strarr[i++], brr, arr);
-	check_if_solved(brr, arr + 1);
+	check_if_solved(brr, arr);
 	free(brr);
 }
